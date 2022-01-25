f8e5383ec47b0bc3049d65a3ff056706023fed62
Nikita Kuznetsov <livisback@yandex.ru>
2022-01-25 21:37:46 +0300

    add button in dom

--- upload.js
+++ upload.js
@@ -1,5 +1,7 @@
 export function upload(selector) {
     const input = document.querySelector(selector)
 
-    
+    const open = document.createElement('button')
+    open.classList.add('btn')
+    open.textContent = 'Открыть'
 }
